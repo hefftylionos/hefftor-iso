acdd60aeeeea958a782bd31495542d021340765f
Brad Heffernan <ph3onix83@gmail.com>
2020-02-29 18:04:46 +1030

    updated

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux-hefftor
-iso_label="arcolinux-hefftor-v20.2.16"
+iso_label="arcolinux-hefftor-v20.2.29"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.2.16"
+iso_version="v20.2.29"
 install_dir=arch
 work_dir=work
 out_dir=out
